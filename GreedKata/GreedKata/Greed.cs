e42409d386c5a16ebfaab90005a70c1e1408440c
Rich <richhildebrand@gmail.com>
2013-06-14 18:55:49 -0400

    Red - Gain600PointsFromThreeOrMoreSixes

--- GreedKata/GreedKata/Greed.cs
+++ GreedKata/GreedKata/Greed.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace GreedKata
 {
     public class Greed
     {
+        public int GetPointsFromSixes(List<int> DiceRollResults)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ GreedKata/UnitTest/GreedShould.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using GreedKata;
+using NUnit.Framework;
+
+namespace UnitTest
+{
+    [TestFixture]
+    public class GreedShould
+    {
+        public IEnumerable<TestCaseData> DiceResultsWithThreeOrMoreSixes
+        {
+            get
+            {
+                yield return new TestCaseData(new List<int>{6,6,6,6,6});
+                yield return new TestCaseData(new List<int>{6,6,6,6,2});
+                yield return new TestCaseData(new List<int>{6,6,5,6,4});
+                yield return new TestCaseData(new List<int>{6,3,6,6,6});
+                yield return new TestCaseData(new List<int>{6,2,4,6,6});
+            }
+        }
+
+        // If you can fix this on your refactor please do so!
+        // I wanted to use test case, but it seemed unhappy with me.
+        [TestCaseSource("DiceResultsWithThreeOrMoreSixes")]
+        public void Gain600PointsFromThreeOrMoreSixes(List<int> diceRollResults)
+        {
+            var greed = new Greed();
+            var expectedPoints = 600;
+
+            var actualPoints = greed.GetPointsFromSixes(diceRollResults);
+
+            Assert.AreEqual(expectedPoints, actualPoints);
+
+        }
+    }
+}
