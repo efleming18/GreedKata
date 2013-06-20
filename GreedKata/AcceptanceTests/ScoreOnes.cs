482dcaa9a26149213b2ea731729ec40d70450362
Rich <richhildebrand@gmail.com>
2013-06-20 19:55:29 -0400

    Refactor - Moved Acceptance Test

---
+++ GreedKata/AcceptanceTests/ScoreOnes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using GreedKata;
+using NUnit.Framework;
+
+namespace AcceptanceTests
+{
+    [TestFixture]
+    public class ScoreOnes
+    {
+        //This could probably use a better name/be moved to another file?
+        //Yes :)
+        [Test]
+        public void GivenOneOne_WhenDiceAreRolled_ThenReturnOneHundredPoints()
+        {
+            var expectedPoints = 100;
+            var testRoll = new List<int> { 1, 0, 0, 0, 0 };
+            var greed = new Greed();
+
+            var actualPoints = greed.GetTotalPoints(testRoll);
+
+            Assert.AreEqual(expectedPoints, actualPoints);
+        }
+
+    }
+}
--- GreedKata/UnitTest/GreedTests/SumOddSidesShould.cs
+++ GreedKata/UnitTest/GreedTests/SumOddSidesShould.cs
@@ -100,19 +100,5 @@ namespace UnitTest.GreedTests
 
             Assert.AreEqual(expectedPoints, actualPoints);
         }
-
-        //This could probably use a better name/be moved to another file?
-        [Test]
-        public void ActuallyReturnOneHundredPointsIfASingleOneIsScored()
-        {
-            var expectedPoints = 100;
-            var testRoll = new List<int> { 1, 0, 0, 0, 0 };
-            var greed = new Greed();
-
-            var actualPoints = greed.GetTotalPoints(testRoll);
-
-            Assert.AreEqual(expectedPoints, actualPoints);
-
-        }
     }
 }
