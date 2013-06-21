aa9ef5776683da9bb05ac30db8f236b0a3d7fd35
Rich <richhildebrand@gmail.com>
2013-06-21 18:59:24 -0400

    Red - Added acceptance test to guide unit testing

--- GreedKata/AcceptanceTests/ScoreOnes.cs
+++ GreedKata/AcceptanceTests/ScoreOnes.cs
@@ -23,13 +23,24 @@ namespace AcceptanceTests
             Assert.AreEqual(expectedPoints, actualPoints);
         }
 
-        //Since you have actually done the specflow you probably already know this, but just to clarify!
-        //This test will stay red until we write enough unit tests TDD style to make it green :)
         [Test]
         public void GivenOnlyTwoOnes_WhenDiceAreRolled_ThenReturnTwoHundredPoints()
         {
             var expectedPoints = 200;
             var testRoll = new List<int> { 1, 1 };
+            var diceScorer = new DiceScorer();
+            var greed = new Greed(diceScorer);
+
+            var actualPoints = greed.GetTotalPoints(testRoll);
+
+            Assert.AreEqual(expectedPoints, actualPoints);
+        }
+
+        [Test]
+        public void GivenOnlyThreeOnes_WhenDiceAreRolled_ThenReturnOneThousandPoints()
+        {
+            var expectedPoints = 1000;
+            var testRoll = new List<int> { 1, 1, 1 };
             var diceScorer = new DiceScorer();
             var greed = new Greed(diceScorer);
 
