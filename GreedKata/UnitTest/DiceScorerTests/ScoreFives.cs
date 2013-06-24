06452481cc806a070483603a41566fd57f055df5
Rich <richhildebrand@gmail.com>
2013-06-23 20:42:08 -0400

    Red - Multiple Red added to comply with ATDD

--- GreedKata/UnitTest/DiceScorerTests/ScoreFives.cs
+++ GreedKata/UnitTest/DiceScorerTests/ScoreFives.cs
@@ -16,9 +16,21 @@ namespace UnitTest.DiceScorerTests
         {
             var diceToScore = new List<int> { 5, 0, 0, 0, 0 };
             var diceScorer = new DiceScorer();
+
             var actualValue = diceScorer.ScoreFives(diceToScore);
 
             Assert.AreEqual(0, actualValue);
+        }
+
+        [Test]
+        public void GivenThreeFives_ThenReturnFiveHundredPoints()
+        {
+            var diceToScore = new List<int> { 5, 5, 5 };
+            var diceScorer = new DiceScorer();
+
+            var actualValue = diceScorer.ScoreFives(diceToScore);
+
+            Assert.AreEqual(500, actualValue);
         }
     }
 }
--- GreedKata/UnitTest/DiceScorerTests/ScoreThrees.cs
+++ GreedKata/UnitTest/DiceScorerTests/ScoreThrees.cs
@@ -11,6 +11,10 @@ namespace UnitTest.DiceScorerTests
     {
         private DiceScorer _diceScorer;
 
+        private static List<int>[] diceRollsWithMoreThanThreeThrees = { new List<int> { 3, 3, 3, 3 },
+                                                                        new List<int> { 3, 3, 3, 3, 3 }
+                                                                      };
+
         [SetUp]
         public void SetUp()
         {
@@ -30,6 +34,24 @@ namespace UnitTest.DiceScorerTests
         public void GivenThreeThrees_ThenReturnThreeHundredPoints()
         {
             var diceToScore = new List<int> { 3, 3, 3, 0, 0 };
+            var actualScore = _diceScorer.ScoreThrees(diceToScore);
+
+            Assert.AreEqual(300, actualScore);
+        }
+
+        //Do you think we should write all of the acceptance tests for the entire feature?
+        //That would probably avoid overlooking test cases since we would still have failing tests.
+        //However, we might lose a bit of focus since we don't have one clear acceptance test to make pass.
+
+        //Or do you not like the acceptance tests? You seems to be forgetting about them here
+        //but you seem to like them with the javaScript. My guess was that you are just running tests from
+        //the unit test project so you aren't seeing them?
+
+        //Whatever works for me =D
+        [Test]
+        [TestCaseSource("diceRollsWithMoreThanThreeThrees")]
+        public void GivenMoreThanThreeThrees_WhenDiceAreRolled_ReturnThreeHundredPoints(List<int> diceToScore)
+        {
             var actualScore = _diceScorer.ScoreThrees(diceToScore);
 
             Assert.AreEqual(300, actualScore);
