2af4a68eeea29f8507dffe2874cb18d34bccb8c4
Rich <richhildebrand@gmail.com>
2013-06-27 19:21:02 -0400

    Refactor - extracted duplicate logic

--- GreedKata/GreedKata/DiceScorer.cs
+++ GreedKata/GreedKata/DiceScorer.cs
@@ -6,52 +6,31 @@ namespace GreedKata
 {
     public class DiceScorer : IDiceScorer
     {
+        private readonly int _sizeOFSet = 3;
+
         public int ScoreOnes(List<int> diceRoll)
         {
-            var numberOfOnes = diceRoll.Count(dr => dr == 1);
-            if (numberOfOnes > 2)
-            {
-                numberOfOnes = (numberOfOnes - 3);
-                return 1000 + (numberOfOnes * 100);
-            }
-            return numberOfOnes * 100;
+            var extraMultiplyerForOnes = 10;
+            var total = StandardScoring(diceRoll, 1) * extraMultiplyerForOnes;
+            total += SingleDiceScoring(diceRoll, 1) * extraMultiplyerForOnes;
+            return total;
         }
 
         public int ScoreThrees(List<int> diceRoll)
         {
-            var numberOfThrees = diceRoll.Count(dr => dr == 3);
-            var total = 0;
-            if (numberOfThrees >= 3)
-            {
-                total = 300;
-            }
+            return StandardScoring(diceRoll, 3);
+        }
+
+        public int ScoreFives(List<int> diceRoll)
+        {
+            var total = StandardScoring(diceRoll, 5);
+            total += SingleDiceScoring(diceRoll, 5);
             return total;
         }
 
-        public int ScoreFives(List<int> _diceRoll)
+        public int ScoreTwos(List<int> diceRoll)
         {
-            var numberOfFives = _diceRoll.Count(dr => dr == 5);
-            var total = 0;
-            if (numberOfFives < 3)
-            {
-                total = numberOfFives * 50;
-            }
-            if (numberOfFives >= 3)
-            {
-                numberOfFives = numberOfFives - 3;
-                total = 500 + (numberOfFives * 50);
-            }
-            return total;
-        }
-
-        public int ScoreTwos(List<int> _diceRoll)
-        {
-            var numberOfTwos = _diceRoll.Count(dr => dr == 2);
-            if (numberOfTwos >= 3)
-            {
-                return 200;
-            }
-            return 0;
+            return StandardScoring(diceRoll, 2);
         }
 
         public int ScoreFours(List<int> _diceRoll)
@@ -62,6 +41,20 @@ namespace GreedKata
         public int ScoreSixes(List<int> _diceRoll)
         {
             return 0;
-        }       
+        }   
+    
+        private int StandardScoring(List<int> diceRoll, int sideToScore)
+        {
+            int numberOfTimesSideWasRolled = diceRoll.Count(die => die == sideToScore);
+            int setsOfThree = numberOfTimesSideWasRolled / _sizeOFSet;
+            return setsOfThree * sideToScore * 100;
+        }
+
+        private int SingleDiceScoring(List<int> diceRoll, int sideToScore)
+        {
+            int numberOfTimesSideWasRolled = diceRoll.Count(die => die == sideToScore);
+            int numberOfSingleDiceToScore = numberOfTimesSideWasRolled % _sizeOFSet;
+            return numberOfSingleDiceToScore * sideToScore * 10;
+        }
     }
 }
