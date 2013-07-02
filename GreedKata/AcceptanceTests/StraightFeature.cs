09ebcdab312d88acd61c7b7355268951dc4989b8
Rich <richhildebrand@gmail.com>
2013-07-01 22:19:39 -0400

    Red - Cause I said!

--- GreedKata/AcceptanceTests/StraightFeature.cs
+++ GreedKata/AcceptanceTests/StraightFeature.cs
@@ -12,16 +12,17 @@ namespace AcceptanceTests
     class StraightFeature
     {
         //Uncomment this for your red!
-        //[Test]
-        //public void GivenStraight_WhenDiceAreRolled_ThenReturnTwelveHundredPoints()
-        //{
-        //    var diceToScore = new List<int> { 1, 2, 3, 4, 5, 6 };
-        //    var diceScorer = new DiceScorer();
-        //    var greed = new Greed(diceScorer);
+        // woo ty!
+        [Test]
+        public void GivenStraight_WhenDiceAreRolled_ThenReturnTwelveHundredPoints()
+        {
+            var diceToScore = new List<int> { 1, 2, 3, 4, 5, 6 };
+            var diceScorer = new DiceScorer();
+            var greed = new Greed(diceScorer);
 
-        //    var actualValue = greed.GetTotalPoints(diceToScore);
+            var actualValue = greed.GetTotalPoints(diceToScore);
 
-        //    Assert.AreEqual(1200, actualValue);
-        //}
+            Assert.AreEqual(1200, actualValue);
+        }
     }
 }
